158f2baf2e5443c97fe95ff25df2ffe37379e945
Utkarsh Gupta <utkarsh.gupta@setindia.com>
2026-05-21 23:11:20 +0530

    Module js

---
+++ 6_Modulejs/a.js
@@ -0,0 +1,10 @@
+export var x = 10;
+export let y = 20;
+
+export function sum(a, b) {
+  return a + b;
+}
+
+export default function Main() {
+  console.log("Start ");
+}
---
+++ 6_Modulejs/b.js
@@ -0,0 +1,9 @@
+import * as A from "./a.js";
+import Main from "./a.js";
+
+console.log(A);
+let p = 20;
+
+const o = A.sum(A.x, A.y);
+console.log(p, o);
+Main();
